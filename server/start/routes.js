01b75e4344cf5fd8b4c660fce67353d862344396
Jirachot Saonram <109726423+Jirachotsaonram@users.noreply.github.com>
2025-01-31 12:27:44 +0700

    sdf

---
+++ server/start/routes.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+Route.get('/', () => {
+  Route.post('api/auth/register', 'UserController.register')
+})
+  .prefix('api/v1')
